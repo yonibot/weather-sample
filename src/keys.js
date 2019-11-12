aba10c4d4ce0761a4355d0f39e8c22b3072a1326
Yoni <code@yoni.fastmail.com>
2019-11-12 20:59:14 +0200

    check in keys

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,8 @@
 // https://developer.accuweather.com/accuweather-locations-api/apis/get/locations/v1/cities/geoposition/search
 
 const keys = {
+    openweather:  '',
+    opencagedata: '',
 }
 
 export default keys;
