d120efa8a46e8eeb77b29c515375e12ff5ad4bc4
Yoni <code@yoni.fastmail.com>
2019-11-18 20:06:06 +0200

    everything inline now

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { geolocated } from 'react-geolocated';
-import { useWeatherService } from './useWeatherService';
 import { TEMPERATURE_UNITS, getOtherTemperatureUnit } from './utils/weather';
 import './App.css';
+import keys from './keys';
 
+const apiKey = keys.openweather;
 
 function App({coords}) {
-  const [ weather, getWeather ] = useWeatherService();
 
   const [ currentUnit, setUnit ] = useState(TEMPERATURE_UNITS.metric);
-
+  const [ weather, setWeather ] = useState();
 
   useEffect(() => {
     if (coords) {
-      const { latitude: lat, longitude: lon } = coords;
-      getWeather(lat, lon, currentUnit.sysName);
+      const fetchWeather = async () => {
+        const { latitude: lat, longitude: lon } = coords;
+        const unit = currentUnit.sysName.toLowerCase();
+        const localWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${unit}&APPID=${apiKey}`);
+        const localWeatherJson = await localWeather.json();
+        const { main: { temp }} = localWeatherJson;
+        setWeather(Math.round(temp));
+      }
+      fetchWeather();
     }
   }, [coords, currentUnit]);
 
@@ -27,13 +34,11 @@ function App({coords}) {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Good morning!</h2>
+        <h2>Hello!</h2>
         <div>
-          { weather && (
             <div>
               It is <span className="em">{weather}</span> {currentUnit.suffix}.
             </div>
-          )}
         </div>
         <br />
         <a className="switch-section" onClick={toggleTempUnit}>Switch to { otherTempUnit }</a>
